d41eaa6fb68ad53e55e301700f3e610ae2ca4b53
Thijs Raymakers <thijs@raymakers.nl>
2020-05-04 12:30:15 +0200

    Add cosine similarity method

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import fasttext
 import fasttext.util
+import numpy
 from typing import List, AnyStr
 
 
@@ -11,8 +12,15 @@ def get_languages(language_codes: List[AnyStr]):
         fasttext.util.download_model(language, if_exists='ignore')
 
 
+def cosine_similarity(a, b):
+    """Calculate the cosine similarity between two vectors. The definition is based on
+     https://en.wikipedia.org/wiki/Cosine_similarity and the snippet of code is based on
+     https://stackoverflow.com/questions/18424228/cosine-similarity-between-2-number-lists/43043160#43043160"""
+    return numpy.dot(a, b) / (numpy.linalg.norm(a) * numpy.linalg.norm(b))
+
+
 def main():
-    get_languages(['en', 'nl'])
+    get_languages(['en'])
     pass
 
 
