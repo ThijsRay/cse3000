32134257b98c2c5ee723d86da3b23dbd9aac47a8
Thijs Raymakers <thijs@raymakers.nl>
2020-05-04 17:39:35 +0200

    Write result to file in output directory

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import sys
 from multiprocessing import Pool
 from typing import List, AnyStr
 
+import string
 import fasttext
 import fasttext.util
 from numpy import dot, ndarray, float32
@@ -49,11 +50,16 @@ def get_languages(language_codes: List[AnyStr]):
         fasttext.util.download_model(language, if_exists='ignore')
 
 
+# Use global variables here (oops) to allow the worker function to access these variables.
 global current_model, current_man_vec, current_woman_vec
 
 
 def worker(word: AnyStr) -> (AnyStr, float32):
     """Worker function for the pool cannot be inner function because it cannot be pickled that way"""
+    # Check if there is any punctuation in the word. If there is, skip it. This used `string.punctuation` minus `-`,
+    # since this symbol can occur in actual words
+    if any((c in "!\"#$%&'()*+,./:;<=>?@[\\]^_`{|}~.") for c in word):
+        return None, None
     word_vector = current_model.get_word_vector(word)
     diff_man = cosine_similarity(word_vector, current_man_vec)
     diff_woman = cosine_similarity(word_vector, current_woman_vec)
@@ -77,26 +83,32 @@ def perform_calculation(language_codes: List[AnyStr]):
 
         with Pool() as pool:
             it = pool.imap(func=worker, iterable=current_model.get_words(), chunksize=100)
-            while True:
-                try:
-                    word, diff = next(it)
-                    if "," in word:
-                        continue
-                    print(f"{word},{int(diff * 1e10)}")
+            with open(f"output/{translation.language}.txt", "a") as f:
+                while True:
+                    try:
+                        word, diff = next(it)
+                        if word is None:
+                            continue
+                        print(f"{word},{int(diff * 1e10)}", file=f)
 
-                    # Update percentage
-                    amount_of_words_done += 1
-                    if amount_of_words_done % 10000 == 0:
-                        eprint(" " * 40, end='\r')
-                        percentage = round((amount_of_words_done / amount_of_words) * 100, 2)
-                        f_string = f"{percentage}%\t of {translation.language}\t" \
-                                   f"Q={amount_of_words_done}\t" \
-                                   f"T={amount_of_words}"
-                        eprint(f_string, end='\r')
-                except StopIteration:
-                    break
+                        # Update and print percentage
+                        amount_of_words_done += 1
+                        print_status(translation.language, amount_of_words_done, amount_of_words)
+                    except StopIteration:
+                        break
+        print(f"\nFinished {translation.language}")
 
-        eprint(f"\nFinished {translation.language}")
+
+def print_status(language: AnyStr, done: int, total: int):
+    """Print the status of this language. Only do it every so ofter, to avoid spending too much resources on the
+    printing."""
+    if done % 10000 == 0:
+        eprint(" " * 40, end='\r')
+        percentage = round((done / total) * 100, 2)
+        f_string = f"{percentage}%\t of {language}\t" \
+                   f"Q={done}\t" \
+                   f"T={total}"
+        eprint(f_string, end='\r')
 
 
 def cosine_similarity(a: ndarray, b: ndarray) -> float32:
@@ -107,7 +119,7 @@ def cosine_similarity(a: ndarray, b: ndarray) -> float32:
 
 
 def main():
-    languages: List[AnyStr] = ['en']
+    languages: List[AnyStr] = ['en', 'nl', 'sv', 'es']
     get_languages(languages)
     perform_calculation(languages)
 
